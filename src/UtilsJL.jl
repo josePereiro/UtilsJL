c715c7e3199a3b479c1b3a7d2b61f40d0936ae4f
josePereiro <jose.pereiro.lab@gmail.com>
2020-10-11 23:13:00 -0400

    Add mypkg_template

--- src/UtilsJL.jl
+++ src/UtilsJL.jl
@@ -5,12 +5,14 @@ module UtilsJL
     import SparseArrays: AbstractSparseArray, sparse, issparse
     import BSON
     import Serialization: serialize, deserialize
-    import DrWatson: wload, tagsave
+    import DrWatson: wload, tagsave, wsave
+    import PkgTemplates: Git, Template, generate
 
     include("commons.jl")
     include("compress.jl")
     include("print_inmw.jl")
     include("cache.jl")
     include("save_load_data.jl")
+    include("mypkg_generate.jl")
 
 end # module
