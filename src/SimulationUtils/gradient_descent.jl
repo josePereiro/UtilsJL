e790583bdcff517573f603752518bde951b36403
josePereiro <jose.pereiro.lab@gmail.com>
2021-03-15 23:13:58 -0400

    Redo gradient_descent, add GDModel, heavy feedback

--- src/SimulationUtils/gradient_descent.jl
+++ src/SimulationUtils/gradient_descent.jl
@@ -1,119 +1,230 @@
-# Vectorize form
-_default_Err_fun(target::Vector) = (fᵢ) -> abs.(target .- fᵢ) ./ ifelse.(iszero.(target), 1.0, abs.(target))
-_default_break_cond(th) = (ϵᵢ) -> maximum(ϵᵢ) < th 
-function grad_desc_vec(f::Function;
-        target::Vector,
-        x0::Vector, x1::Vector,
-        maxΔ::Vector = abs.(x0 - x1), th = 1e-5,
-        maxiters::Int = 1000, 
-        toshow::Vector = [],
-        Err = _default_Err_fun(target),
-        break_cond = _default_break_cond(th),
+## ------------------------------------------------------------------------------
+# Container
+mutable struct GDModel{T}
+    # current state
+    iter::Int
+    xi::T
+    fi::T
+    ϵi::T
+    ϵii::T
+    Δx::T
+    sense::T
+
+    # params
+    target::T
+    maxΔx::T
+    maxiter::Int
+    gdth::Real
+    verbose::Bool
+end
+
+# constructor
+function GDModel(;
+        target, maxΔx, 
+        maxiter = Int(1e4), 
+        it0 = 1, 
+        gdth,
         verbose = true,
-        oniter::Function = (it, fᵢ, xᵢ, Δx) -> (false, zero(x0))
     )
 
-    # initializing
-    xᵢ₋₁, xᵢ = x0, x1
-    fᵢ₋₁ = f(xᵢ₋₁)
-    ϵᵢ₋₁ = Err(fᵢ₋₁)
-    Δx = zero(xᵢ)
-    sense = ones(length(target))
+    iter = Int(it0)
+    maxiter = Int(maxiter)
+    xi = zero(target)
+    fi = zero(target)
+    ϵi = zero(target)
+    ϵii = zero(target)
+    Δx = zero(target)
+    sense = zero(target)
 
-    verbose && (prog = ProgressThresh(th, "Grad desc: "))
-    for it in 1:maxiters
-        
-        xᵢ += Δx
-        fᵢ = f(xᵢ)
-        ϵᵢ = Err(fᵢ)
-        sense .*= -sign.(ϵᵢ .- ϵᵢ₋₁)
-        Δx = sense .* maxΔ .* min.(1.0, ϵᵢ)
+    GDModel{typeof(target)}(
+        # state
+        iter, xi, fi, ϵi, ϵii, Δx, sense,
+        # params
+        target, maxΔx, maxiter, gdth, verbose
+    )
+end
+
+gd_value(gdmodel::GDModel) = gdmodel.xi
+
+## ------------------------------------------------------------------------------
+# Vectorize version
+_default_Err_fun(gdmodel::GDModel{T}) where T<:AbstractArray = 
+    abs.(gdmodel.target - gdmodel.fi) ./ ifelse.(iszero.(gdmodel.target), 1.0, abs.(gdmodel.target))
+_default_break_cond(gdmodel::GDModel{T}) where T<:AbstractArray = 
+    maximum(gdmodel.ϵi) < gdmodel.gdth 
+_default_get_step(gdmodel::GDModel{T}) where T<:AbstractArray = 
+    gdmodel.sense .* gdmodel.maxΔx .* min.(1.0, gdmodel.ϵi)
+function _default_get_sense(gdmodel::GDModel{T}) where T<:AbstractArray 
+    Δϵ = gdmodel.ϵi .- gdmodel.ϵii
+    zeros = iszero.(Δϵ)
+    sense = gdmodel.sense .* -sign.(gdmodel.ϵi .- gdmodel.ϵii)
+    if any(zeros)
+        rsense = rand(-1:1, length(Δϵ))
+        sense = ifelse.(zeros, rsense, sense)
+    end
+    return sense
+end
+
+    
+function grad_desc_vec!(f::Function, gdmodel::GDModel{T};
+        x0::T, x1::T,
+        toshow::Vector = [],
+        Err::Function = _default_Err_fun,
+        break_cond::Function = _default_break_cond,
+        oniter!::Function = (dgmodel) -> false,
+        get_sense::Function = _default_get_sense,
+        get_step::Function = _default_get_step,
+    ) where {T<:AbstractArray}
+
+    # initializing
+    gdmodel.xi .= x0
+    gdmodel.fi .= f(gdmodel)
+    gdmodel.ϵii .= Err(gdmodel)
+    gdmodel.sense .= ones(length(gdmodel.target))
+    gdmodel.Δx .= zero(gdmodel.xi)
+    gdmodel.xi .= x1
+
+    prog = ProgressThresh(gdmodel.gdth, "Grad desc: ")
+    it0 = gdmodel.iter
+    while true
+
+        gdmodel.xi += gdmodel.Δx
+        gdmodel.fi .= f(gdmodel)
+        gdmodel.ϵi .= Err(gdmodel)
+        gdmodel.sense .= get_sense(gdmodel)
+        gdmodel.Δx .= get_step(gdmodel)
         
         # callback
-        ret, val = oniter(it, fᵢ, xᵢ, Δx)
-        ret && return val
+        ret = oniter!(gdmodel)
+        ret && return gdmodel
         
-        if verbose
-            maxϵᵢ = maximum(ϵᵢ)
+        if gdmodel.verbose
+            maxϵᵢ = maximum(gdmodel.ϵi)
             update!(prog, maxϵᵢ; showvalues = vcat(
                     [
-                        ("it", it),
+                        ("it", gdmodel.iter),
                         ("maxϵᵢ", maxϵᵢ),
-                        ("ϵᵢ", ϵᵢ),
-                        ("sense", sense),
-                        ("xᵢ", xᵢ),
-                        ("t", target),
-                        ("fᵢ", fᵢ),
+                        ("ϵᵢ", gdmodel.ϵi),
+                        ("sense", gdmodel.sense),
+                        ("xᵢ", gdmodel.xi),
+                        ("t", gdmodel.target),
+                        ("fi", gdmodel.fi),
                     ], toshow
                 )
             )
         end
 
-        ϵᵢ₋₁ = ϵᵢ
+        gdmodel.ϵii .= gdmodel.ϵi
 
         # break condition
-        break_cond(ϵᵢ) && break
-        
-    end
-    verbose && finish!(prog)
+        break_cond(gdmodel) && break
+        gdmodel.iter >= gdmodel.maxiter &&  break
 
-    return xᵢ
+        gdmodel.iter += 1
+    end
+    gdmodel.verbose && finish!(prog)
+
+    return gdmodel
 end
 
-## ----------------------------------------------------------------------------
-_default_Err_fun(target::Real) = (fᵢ) -> abs(target - fᵢ) / ifelse(iszero(target), 1.0, abs(target))
-
-function grad_desc(f::Function;
-        target::Real,
-        x0::Real, x1::Real,
-        maxΔ::Real = abs(x0 - x1), th = 1e-5,
-        maxiters::Int = Int(1e4), 
+function grad_desc_vec(f::Function;
+        x0::T, x1::T,
+        target::T, 
+        maxΔx::T = abs.(x0 - x1), 
+        gdth::Real = 1e-5, 
+        maxiter = Int(1000), 
+        it0 = 1, 
         verbose = true,
+        kwargs...
+    ) where {T<:AbstractArray}
+
+    gdmodel = GDModel(;target, maxΔx, maxiter, it0, gdth, verbose)
+    grad_desc_vec!(f, gdmodel; x0, x1, kwargs...)
+
+end
+
+## ------------------------------------------------------------------------------
+# Scalar version
+_default_Err_fun(gdmodel::GDModel{T}) where T<:Real = 
+    abs(gdmodel.target - gdmodel.fi) / ifelse(iszero(gdmodel.target), 1.0, abs(gdmodel.target))
+_default_break_cond(gdmodel::GDModel{T}) where T<:Real = gdmodel.ϵi < gdmodel.gdth 
+_default_get_step(gdmodel::GDModel{T}) where T<:Real = gdmodel.sense * gdmodel.maxΔx * min.(1.0, gdmodel.ϵi)
+function _default_get_sense(gdmodel::GDModel{T}) where T<:Real 
+    Δϵ = gdmodel.ϵi - gdmodel.ϵii
+    iszero(Δϵ) ? rand(-1:1) : gdmodel.sense * -sign(gdmodel.ϵi - gdmodel.ϵii)
+end
+
+function grad_desc!(f::Function, gdmodel::GDModel{T};
+        x0::T, x1::T,
         toshow::Vector = [],
-        Err::Function = _default_Err_fun(target),
-        oniter::Function = (it, fᵢ, xᵢ, sense) -> (false, zero(x0))
-    )
+        Err::Function = _default_Err_fun,
+        break_cond::Function = _default_break_cond,
+        oniter!::Function = (dgmodel) -> false,
+        get_sense::Function = _default_get_sense,
+        get_step::Function = _default_get_step
+    ) where {T<:Real}
 
     # initializing
-    xᵢ₋₁, xᵢ = x0, x1
-    fᵢ₋₁ = f(xᵢ₋₁)
-    ϵᵢ₋₁ = Err(fᵢ₋₁)
-    sense = one(target)
-    Δx = 0.0
+    gdmodel.xi = x0
+    gdmodel.fi = f(gdmodel)
+    gdmodel.ϵii = Err(gdmodel)
+    gdmodel.sense = one(gdmodel.target)
+    gdmodel.Δx = zero(gdmodel.xi)
+    gdmodel.xi = x1
 
-    verbose && (prog = ProgressThresh(th, "Grad desc: "))
-    for it in 1:maxiters
-        
-        xᵢ += Δx
-        fᵢ = f(xᵢ)
-        ϵᵢ = Err(fᵢ)
-        sense *= ϵᵢ > ϵᵢ₋₁ ? -1.0 : 1.0
-        iszero(sense) && error("sense == 0. Descend gets stocked, target unreachable!!!")
-        Δx = sense * maxΔ * min(1.0, ϵᵢ)
+    prog = ProgressThresh(gdmodel.gdth, "Grad desc: ")
+    it0 = gdmodel.iter
+    while true
+
+        gdmodel.xi += gdmodel.Δx
+        gdmodel.fi = f(gdmodel)
+        gdmodel.ϵi = Err(gdmodel)
+        gdmodel.sense = get_sense(gdmodel)
+        gdmodel.Δx = get_step(gdmodel)
         
         # callback
-        ret, val = oniter(it, fᵢ, xᵢ, Δx)
-        ret && return val
+        ret = oniter!(gdmodel)
+        ret && return gdmodel
         
-        ϵᵢ < th && break
-
-        verbose && update!(prog, ϵᵢ; showvalues = vcat(
-                [
-                    ("it", it),
-                    ("ϵᵢ", ϵᵢ),
-                    ("sense", sense),
-                    ("xᵢ", xᵢ),
-                    ("Δx", Δx),
-                    ("t", target),
-                    ("fᵢ", fᵢ),
-                ], toshow
+        if gdmodel.verbose
+            update!(prog, gdmodel.ϵi; showvalues = vcat(
+                    [
+                        ("it", gdmodel.iter),
+                        ("ϵᵢ", gdmodel.ϵi),
+                        ("sense", gdmodel.sense),
+                        ("xᵢ", gdmodel.xi),
+                        ("t", gdmodel.target),
+                        ("fi", gdmodel.fi),
+                    ], toshow
+                )
             )
-        )
+        end
 
-        ϵᵢ₋₁ = ϵᵢ
+        gdmodel.ϵii = gdmodel.ϵi
 
+        # break condition
+        break_cond(gdmodel) && break
+        gdmodel.iter >= gdmodel.maxiter &&  break
+
+        gdmodel.iter += 1
     end
-    verbose && finish!(prog)
+    gdmodel.verbose && finish!(prog)
 
-    return xᵢ
+    return gdmodel
+end
+
+function grad_desc(f::Function;
+        x0::T, x1::T,
+        target::T, 
+        maxΔx::T = abs.(x0 - x1), 
+        gdth::Real = 1e-5, 
+        maxiter = Int(1000), 
+        it0 = 1, 
+        verbose = true,
+        kwargs...
+    ) where {T<:Real}
+
+    gdmodel = GDModel(;target, maxΔx, maxiter, it0, gdth, verbose)
+    grad_desc!(f, gdmodel; x0, x1, kwargs...)
+
 end
--- test/SimulationsUtils_tests/grad_desc_tests.jl
+++ test/SimulationsUtils_tests/grad_desc_tests.jl
@@ -1,27 +1,41 @@
 function test_grad_desc()
 
     # Vectorize
-    for i in 1:10
+    for i in 1:1000
         target = [10.0 * rand() + 5.0, 10.0 * rand() + 5.0]
         x0 = [0.0, 0.0]
-        x1 = [0.1, 0.1]
-        maxΔ = [1.0, 1.0]
-        f(xs) = (xs .^ [1, 2]) .+ reverse(xs)
-        th = 1e-10
-        xs = Utils.grad_desc_vec(f;target, x0, x1, maxΔ, th)
-        @test all(isapprox.(f(xs), target; atol = 1e-8))
+        rexps = rand(1:10, 2)
+        maxΔx = 1.0 ./ rexps
+        x1 = x0 .+ maxΔx .* 0.1
+        gdth = 1e-10
+
+        function up_fun(gdmodel) 
+            xs = UtilsJL.gd_value(gdmodel)
+            xs .^ rexps
+        end
+        gdmodel = UtilsJL.grad_desc_vec(up_fun; target, x0, x1, maxΔx, gdth)
+        @test all(isapprox.(up_fun(gdmodel), target; atol = 1e-8))
     end
 
-    # Vectorize
-    for i in 1:10
-        target = 10.0 * rand() + 5.0
-        x0 = 0.0
-        x1 = 0.1
-        maxΔ = 1.0
-        f(xs) = xs ^ 2 
-        th = 1e-10
-        xs = Utils.grad_desc_vec(f;target, x0, x1, maxΔ, th)
-        @test all(isapprox.(f(xs), target; atol = 1e-8))
+    # Scalar
+    @testset begin
+        for i in 1:1000
+            target = 10.0 * rand() + 5.0
+            x0 = 0.0
+            rexp = rand(1:10)
+            maxΔx = 1.0 / rexp
+            x1 = x0 + maxΔx * 0.1
+            maxiter = 1e4
+            gdth = 1e-10
+
+            function up_fun(gdmodel) 
+                x = UtilsJL.gd_value(gdmodel)
+                x ^ rexp
+            end
+            gdmodel = UtilsJL.grad_desc(up_fun; target, x0, x1, maxΔx, gdth, maxiter)
+            @test all(isapprox.(up_fun(gdmodel), target; atol = 1e-8))
+        end
     end
 
 end
+test_grad_desc()
