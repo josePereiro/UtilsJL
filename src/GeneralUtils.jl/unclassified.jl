a556ce9b690c47b04c39ae557b96ebee0d08f905
josePereiro <jose.pereiro.lab@gmail.com>
2021-03-21 02:09:17 -0400

    Add get!push!

--- src/GeneralUtils.jl/unclassified.jl
+++ src/GeneralUtils.jl/unclassified.jl
@@ -36,3 +36,10 @@ function err_str(err; max_len = 10000)
     s = sprint(showerror, err, catch_backtrace())
     return length(s) > max_len ? s[1:max_len] * "\n[...]" : s
 end
+
+function get!push!(d; kwargs...)
+    for (k, val) in kwargs
+        push!(get!(d, k, []), val)
+    end
+    d
+end
